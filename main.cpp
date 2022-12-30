8692c22579c9c55f93a24cd9de37a13bbba7c1e0
Alessandro Bossi <alebossi96@hotmail.it>
2022-12-30 15:03:39 +0100

    fixed bug initialization photon

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ double henyey_greenstein_F(double theta)
 {
     // Calculate the CDF for the Henyey-Greenstein phase function
     // g is the asymmetry parameter
-    double g = 0.9;
+    double g = 0.1;
     //Martelli's book 9.12 old version
     return (1-g*g)/(2*g)*(1/(1-g) -1/sqrt(1+g*g-2*g*cos(theta)));
 }
@@ -60,19 +60,108 @@ double inverse_transform_sampling(std::function<double(double)> cdf,    // CDF f
 
 
 
+// convert a direction versor to spherical coordinates
+void toSpherical(const Vector& v, double& r, double& theta, double& phi) {
+  r = std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+  theta = std::acos(v.z / r);
+  phi = std::atan2(v.y, v.x);
+}
+
+// convert spherical coordinates to a direction versor
+Vector fromSpherical(double r, double theta, double phi) {
+  Vector v;
+  v.x = r * std::sin(theta) * std::cos(phi);
+  v.y = r * std::sin(theta) * std::sin(phi);
+  v.z = r * std::cos(theta);
+  return v;
+}
+
+// compute the output versor given the input direction versor, the deflection angle, and the azimuth angle
+Vector computeOutputVersor(const Vector& v, double deflectionAngle, double azimuthAngleDeflection) {
+  // convert input direction versor to spherical coordinates
+  double r, theta, phi;
+  toSpherical(v, r, theta, phi);
+  phi+=azimuthAngleDeflection;
+  // modify the polar angle to reflect the deflection angle
+  theta += deflectionAngle;
+
+  // convert modified spherical coordinates to output direction versor
+  return fromSpherical(r, theta, azimuthAngleDeflection);
+}
+
+Vector generateRandomDirection(std::mt19937& rng, const Vector& incomingDirection) {
+  // Generate the scattering angle using the inverse CDF of the provided probability function
+  double deflectionAngle = inverse_transform_sampling(henyey_greenstein_F, rng);//è il coseno
+  std::uniform_real_distribution<double> uniform(0, 1);  // uniform distribution from 0 to 1
+  double u = uniform(rng);
+  // Compute the scattered direction using the incoming direction and the scattering angle
+  /*
+  double cosTheta = cos(theta);
+  double sinTheta = sqrt(1 - cosTheta * cosTheta);
+  double phi =  2 * PI * u;//atan2(incomingDirection.y, incomingDirection.x);//TODO errore qui
+
+  Vector v;
+  v.x = sinTheta * cos(phi);
+  v.y = sinTheta * sin(phi);
+  v.z = cosTheta;
+  */
+  return computeOutputVersor(incomingDirection, deflectionAngle, 2 * PI * u);
+}
+
+
 int main() {//eulero stocastico
-    // Create a random number generator
-    std::ofstream out_file("output.txt");
-    if (!out_file) {
-        std::cerr << "Error opening file" << std::endl;
-        return 1;
+  // Create a random number generator
+  std::mt19937 rng(12345);
+  double radius_detector = 10;
+
+  // Set the parameters for the simulation
+  double mu_s = 1;
+  double stepSize = 0.1;
+  int numSteps = 100;
+  std::vector<int> photon_count(numSteps);
+  int tot = 0;
+  // Initialize the photon
+
+  for( int j = 0; j<1e4; ++j){
+    Photon photon;
+    photon.position.x = 0;
+    photon.position.y = 0;
+    photon.position.z = 0;
+    photon.direction.x = 0;
+    photon.direction.y = 0;
+    photon.direction.z = -1;  
+    // Propagate the photon through the medium
+    for (int i = 0; i < numSteps; ++i) {
+      // Scatter the photon with probability exp(-mu_s * stepSize)
+      double pos_x{photon.position.x}, pos_y{photon.position.y}, pos_z{photon.position.z};
+      if (rand01(rng) < 1-exp(-mu_s * stepSize)) {
+        photon.direction = generateRandomDirection(rng, photon.direction);
+        //std::cout<<photon.direction.x<<" "<<photon.direction.y<<" "<<photon.direction.z<<std::endl;
+      }
+
+      // Propagate the photon in its current direction
+      photon.position.x += stepSize * photon.direction.x;
+      photon.position.y += stepSize * photon.direction.y;
+      photon.position.z += stepSize * photon.direction.z;
+      //std::cout<<photon.position.x<<" "<<photon.position.y<<" "<<photon.position.z<<std::endl;
+      //TODO detector interaction
+      
+      if (pos_z*photon.position.z<0){
+        //std::cout<<"in"<<std::endl;
+        double t =  -photon.position.z/ photon.direction.z;
+        double x_intersection = photon.direction.x*t+ photon.position.x;
+        double y_intersection = photon.direction.y*t+ photon.position.y;
+        if (x_intersection*x_intersection+y_intersection*y_intersection < radius_detector*radius_detector){
+            ++photon_count[i];
+            ++tot;
+            break;
+            }
+        
+        }
     }
-    std::mt19937 rng(12345);
-    for(int i = 0; i<1e5; ++i){
-        double n = inverse_transform_sampling(henyey_greenstein_F, rng);
-        //std::cout<<n*180/3.14<<std::endl;
-        out_file<<n<<std::endl;
-    }
-    out_file.close();
-    return 0;
+    
+    std::cout<<j<<std::endl;
+  }
+  std::cout<<"tot:"<<tot<<std::endl;
+  return 0;
 }
